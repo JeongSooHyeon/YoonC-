3c398d28b62cc4a0a28d2bb154f42479b5503c5a
SooHyeon Jeong <jsh75721@naver.com>
2023-02-15 23:42:42 +0900

    second commit

--- test.cpp
+++ test.cpp
@@ -16,4 +16,5 @@ int main()
     std::cout << "[3, 3, 3] : " << BoxVolume(3, 3, 3) << std::endl;
     std::cout << "[5, 5, D] : " << BoxVolume(5, 5) << std::endl;
     std::cout << "[7, D, D] : " << BoxVolume(7) << std::endl;
+    std::cout << "모우?";
 }
